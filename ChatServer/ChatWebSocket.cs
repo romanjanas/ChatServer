1abb0737fd864edfb2ac7498c3179c059f324ca4
romanjanas <janas.roman@gmail.com>
2016-08-23 23:37:12 +0200

    initial commit - PoC

---
+++ ChatServer/ChatWebSocket.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Web;
+using Owin.WebSocket;
+using System.Text;
+using System.Collections.Concurrent;
+
+namespace ChatServer
+{
+    public class ChatWebSocket : WebSocketConnection
+    {
+        static ConcurrentBag<object> clients = new ConcurrentBag<object>();
+
+        public override void OnOpen()
+        {
+            clients.Add(new Client(this));
+        }
+
+        public override Task OnMessageReceived(ArraySegment<byte> message, WebSocketMessageType type)
+        {
+            var text = Encoding.UTF8.GetString(message.Array, message.Offset, message.Count);
+
+            ProcessMessageFromClient(text);
+            
+            return Task.FromResult(0);
+        }
+
+        public override void OnClose(WebSocketCloseStatus? closeStatus, string closeStatusDescription)
+        {
+            base.OnClose(closeStatus, closeStatusDescription);
+            SendToAllClients("Client disconnected");
+        }
+
+        private void ProcessMessageFromClient(String message)
+        {
+            SendToAllClients(message);
+        }
+
+        private void SendToAllClients(String toSend)
+        {
+            var message = Encoding.UTF8.GetBytes(toSend);
+            foreach (object client in clients)
+            {
+                if (client is Client)
+                {
+                    ((Client)client).connection.SendText(message, true);
+                }
+            }
+        }
+
+        private class Client
+        {
+            public ChatWebSocket connection { get; set; }
+            public String nickname { get; set; }
+            public String color { get; set; }
+
+            public Client(ChatWebSocket connection)
+            {
+                this.connection = connection;
+            }
+        }
+    }
+}
---
+++ ChatServer/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Owin.WebSocket.Extensions;
+
+[assembly: OwinStartup(typeof(ChatServer.Startup))]
+
+namespace ChatServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapWebSocketRoute<ChatWebSocket>("/chatws");
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html";
+                return context.Response.WriteAsync("Continue to <a href='http://chatserver6908.azurewebsites.net/chatRoom.html'>chat room</a>.");
+            });
+        }
+    }
+}
